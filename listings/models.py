a613a9e106c324c71e572ade15cde14834f7e90a
alaametwale <alaameto86@gmail.com>
2025-11-19 12:19:10 +0200

    Add README, models, and serializers

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.CharField(max_length=100)
+
+class Review(models.Model):
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    rating = models.IntegerField()
